7b8fa9e7e98e9f0848a8fcceb71057712a261c47
Capi Etheriel <barraponto@gmail.com>
2025-12-18 13:42:39 -0300

    feat(ai): add middlewares to deal with retries and message history summarization

--- agent.py
+++ agent.py
@@ -1,9 +1,15 @@
 from langchain.agents import create_agent
+from langchain.agents.middleware import (
+    ModelRetryMiddleware,
+    SummarizationMiddleware,
+    ToolRetryMiddleware,
+)
 from langchain_mcp_adapters.client import MultiServerMCPClient
 
 system_prompt = """
 You are a customer support agent.
 You are given a customer query and you need to help the customer with their query.
+Always try to remember the product categories.
 """
 
 
@@ -13,5 +19,14 @@ async def get_agent(client: MultiServerMCPClient):
         model="groq:llama-3.1-8b-instant",
         tools=tools,
         system_prompt=system_prompt,
+        middleware=[
+            ToolRetryMiddleware(max_retries=3),
+            ModelRetryMiddleware(max_retries=3),
+            SummarizationMiddleware(
+                model="groq:llama-3.1-8b-instant",
+                trigger=("messages", 12),
+                keep=("messages", 4),
+            ),
+        ],
     )
     return agent
