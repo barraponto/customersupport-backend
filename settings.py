d989f410dd5648c0858157d68b125eb911de024d
Capi Etheriel <barraponto@gmail.com>
2025-12-18 12:23:33 -0300

    feat(app): add settings model

---
+++ settings.py
@@ -0,0 +1,8 @@
+from pydantic import Field
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict()
+
+    groq_api_key: str = Field(default="")
